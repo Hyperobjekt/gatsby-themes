f6006b3140ac65f61b0bd3879cf062da3c8f9b47
lane <laneolson@gmail.com>
2021-10-12 12:45:58 -0700

    fix: add theme placeholder

---
+++ themes/gatsby-theme-hypercore/src/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from "@material-ui/core"
+
+let theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#333",
+    },
+    secondary: {
+      main: "#999",
+    },
+  },
+})
+
+export default theme
